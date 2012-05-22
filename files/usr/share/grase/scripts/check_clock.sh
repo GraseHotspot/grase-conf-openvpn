d65deb3647f41343d3e4efeed3bba5cd8cb9d3fb
Timothy White <tim@purewhite.id.au>
2012-05-23 07:31:17 +1000

    Have clock check script show what the date was and what it gets set to

--- files/usr/share/grase/scripts/check_clock.sh
+++ files/usr/share/grase/scripts/check_clock.sh
@@ -4,7 +4,9 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 [ `date +%s` -lt `stat -c %Y $0` ] && (
 	echo "Time appears to be in the past, attempting to update the clock"
+	date
 	ntpdate -u pool.ntp.org || true
 	/etc/init.d/ntp restart || true
+	date
 )
 
