9cef5379fd97c4524e16714a4dc43625d35025d0
Tim White <tim@whiteitsolutions.com.au>
2020-06-06 14:22:48 +0800

    Remove package as we are no longer offering a VPN service

---
+++ README.md
@@ -0,0 +1,2 @@
+# NOTICE  - Removal of VPN Remote Access
+See https://grasehotspot.org/2020/06/06/notice-removal-of-vpn-remote-access/ for details as to alternatives to this service
--- debian/grase-conf-openvpn.cron.d
+++
@@ -1,9 +0,0 @@
-# /etc/cron.d/grase-conf-openvpn: crontab fragment for grase-conf-openvpn
-# This ensures that we have correct time as the VPN can not function without
-# the clock being correct (SSL will fail)
-# m h	dom mon dow	command
-
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-## NEEDS USERNAME
-
-*/10 *	* * *	root	/usr/share/grase/scripts/check_clock.sh
